cbb46f8cc8c45826c0ec06fadc5aec2a7b6e8a4c
Sam Title <samstitle@gmail.com>
2025-03-16 21:32:35 -0400

    I can build the npm stuff

--- flake.nix
+++ flake.nix
@@ -2,16 +2,39 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in pkgs.mkShell {
-      buildInputs = with pkgs; [
-        nodejs
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        bundle = import ./. { };
+        imageName = "vite-nix-example";
+        imageCmd = [ "node" "${bundle}/dist/index.html" ];
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            prefetch-npm-deps
+            git
+          ];
+        };
+
+        packages = {
+          vite-nix-example = pkgs.buildNpmPackage {
+            pname = "vite-nix-example";
+            version = "0.0.1";
+            src = ./.;
+            npmDepsHash = "sha256-GdBAQ0EtjBAwjbvgU9PrweV5xnR+94NOhOAPXwWiC1I=";
+            installPhase = ''
+              runHook preInstall
+              mkdir $out
+              mv dist node_modules $out
+              runHook postInstall
+            '';
+          };
+        };
+      });
 }
